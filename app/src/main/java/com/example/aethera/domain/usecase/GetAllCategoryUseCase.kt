ba9ce7cb2f0cb75d08b95ce6c58f76b97f6278ef
Tarun Sharma <tarundadhich05@gmail.com>
2026-05-01 23:57:58 +0530

    refactor: remove Hilt residue and dead code from old architecture
    - Deleted dataModule.kt and UiModule.kt (Hilt @Module classes, redundant with Koin AppModule)
    - Deleted repoImpl.kt and repo.kt (old repo interface/impl, replaced by domain/repository/)
    - Deleted GetAllCategoryUseCase and GetAllProductUseCase (@Inject, not wired into Koin)
    - Deleted old homeScreen.kt (replaced by presentation/home/HomeScreen.kt)

--- app/src/main/java/com/example/aethera/domain/usecase/GetAllCategoryUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aethera.domain.usecase
-
-import com.example.aethera.domain.repo.repo
-import javax.inject.Inject
-
-class GetAllCategoryUseCase @Inject constructor(private val repo: repo){
-
-    fun getAllCategoryUseCase() = repo.getAllCategory()
-}
--- app/src/main/java/com/example/aethera/domain/usecase/GetAllProductUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aethera.domain.usecase
-
-import com.example.aethera.domain.repo.repo
-import javax.inject.Inject
-
-class GetAllProductUseCase @Inject constructor(private val repo : repo) {
-
-    fun getAllProductUseCase() = repo.getAllProducts()
-}
