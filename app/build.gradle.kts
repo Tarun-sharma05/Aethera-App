4321c173fdcfc56358fe53967a23105f101c2bde
Tarun Sharma <tarundadhich05@gmail.com>
2026-05-01 23:59:07 +0530

    build: update gradle config and dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     // Google Services (Firebase)
@@ -30,9 +31,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
+//    kotlinOptions {
+//        jvmTarget = "21"
+//    }
     buildFeatures {
         compose = true
     }
