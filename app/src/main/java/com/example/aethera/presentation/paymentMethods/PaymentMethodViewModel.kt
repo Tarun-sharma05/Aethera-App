4768371eb31c0b399b571aa27260fe7edf41a102
Tarun Sharma <tarundadhich05@gmail.com>
2026-05-18 12:36:26 +0530

    Added PaymentMethodsScreen and PaymentMethodViewModel for App's payment system.

--- app/src/main/java/com/example/aethera/presentation/paymentMethods/PaymentMethodViewModel.kt
+++ app/src/main/java/com/example/aethera/presentation/paymentMethods/PaymentMethodViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.aethera.presentation.paymentMethods
 
+import com.example.aethera.domain.models.Order
+
+
+
+
 class PaymentMethodViewModel {
 }
--- app/src/main/java/com/example/aethera/presentation/paymentMethods/PaymentMethodsScreen.kt
+++ app/src/main/java/com/example/aethera/presentation/paymentMethods/PaymentMethodsScreen.kt
@@ -1,4 +1,11 @@
 package com.example.aethera.presentation.paymentMethods
 
-class PaymentMethodsScreen {
+import androidx.compose.runtime.Composable
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun PaymentMethodsScreen(
+    onBack : () -> Unit,
+//    viewModel: PaymentMethodViewModel = koinViewModel()
+) {
 }
